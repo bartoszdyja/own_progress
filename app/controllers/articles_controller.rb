86e8c7a7c98e2f06c077dddb3ffb2e1e5605038f
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-05-25 16:40:38 +0200

    Added json respond

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,11 @@ class ArticlesController < ApplicationController
 
 
   def index
+    @articles = Article.all
+    respond_to do |format|
+      format.html 
+      format.json {render json: @articles}
+    end 
   end
 
   private
